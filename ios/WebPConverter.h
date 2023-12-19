28429b1bbf8a1ef7b08fb97ac2441665b4312c0a
abhinandvk <abhinandvk41@gmail.com>
2023-12-20 01:48:46 +0530

    updated

---
+++ ios/WebPConverter.h
@@ -0,0 +1,16 @@
+//
+//  WebPConverter.h
+//  gifapp
+//
+//  Created by Abhinand Vk on 19/12/23.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WebPConverter : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios/WebPConverter.m
@@ -0,0 +1,12 @@
+//
+//  WebPConverter.m
+//  gifapp
+//
+//  Created by Abhinand Vk on 19/12/23.
+//
+
+#import "WebPConverter.h"
+
+@implementation WebPConverter
+
+@end
